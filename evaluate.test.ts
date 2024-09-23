ff706631fa59c0fe373a609ddcc98d8793be7cf0
ben <benjamin.p.rosen@gmail.com>
2024-09-23 11:16:12 -0400

    try creating matchers and evaluators

--- evaluate.test.ts
+++ evaluate.test.ts
@@ -3,11 +3,26 @@ import { evaluate } from "./evaluate";
 describe("evaluate", () => {
   test.each([
     ["", ""],
-    ["hello", "hello"],
+    ['""', ""],
+    ["hello", ""],
+    ['"hello"', "hello"],
     ['output "hello"', "hello"],
     ["output 0", "0"],
-  ])("'%s' should return '%s'", async (knife: string, expected: string) => {
-    const result = await evaluate(knife);
-    expect(result).toStrictEqual(expected);
-  });
+    ["output", ""],
+    ["output output", ""],
+    ["output output 0", "0"],
+    ["output input", "", { input: "" }],
+    ["output input", "hello", { input: "hello" }],
+    ["output input.foo", "bar", { input: JSON.stringify({ foo: "bar" }) }],
+  ])(
+    "'%s' should return '%s'",
+    async (
+      knife: string,
+      expected: string,
+      context: { input: string } = { input: "" },
+    ) => {
+      const result = await evaluate(knife, context);
+      expect(result).toStrictEqual(expected);
+    },
+  );
 });
--- evaluate.ts
+++ evaluate.ts
@@ -1,15 +1,62 @@
-export const evaluate = async (knife: string): Promise<string> => {
-  const tokens = knife.split(" ");
-
-  const isOutputCommand = tokens[0] === "output";
-
-  if (isOutputCommand) {
-    const remainingTokens = tokens.slice(1);
-
-    const remainingKnife = remainingTokens.join(" ");
-
-    return await evaluate(remainingKnife);
+export const evaluate = async (
+  value: string,
+  context: { input: string },
+): Promise<string> => {
+  if (isOutputCommand(value)) {
+    return evaluateOutputCommand(value, context);
   }
 
-  return knife;
+  if (isStringLiteral(value)) {
+    return evaluateStringLiteral(value);
+  }
+
+  if (isNumberLiteral(value)) {
+    return evaluateNumberLiteral(value).toString();
+  }
+
+  if (isInputKeyword(value)) {
+    return evaluateInputKeyword(context);
+  }
+
+  return "";
+};
+
+const isOutputCommand = (command: string): boolean => {
+  const outputCommandRegularExpression = /^output\s/;
+  return outputCommandRegularExpression.test(command);
+};
+
+const evaluateOutputCommand = async (
+  command: string,
+  context: { input: string },
+): Promise<string> => {
+  const outputCommandPrefix = "output ";
+  const restOfCommand = command.slice(outputCommandPrefix.length);
+  return await evaluate(restOfCommand, context);
+};
+
+const isStringLiteral = (value: string): boolean => {
+  const stringLiteralRegularExpression = /^".*"$/;
+  return stringLiteralRegularExpression.test(value);
+};
+
+const evaluateStringLiteral = (value: string): string => {
+  return value.slice(1, -1);
+};
+
+const isNumberLiteral = (value: string): boolean => {
+  const numberLiteralRegularExpression = /^\d+$/;
+  return numberLiteralRegularExpression.test(value);
+};
+
+const evaluateNumberLiteral = (value: string): number => {
+  return JSON.parse(value);
+};
+
+const isInputKeyword = (value: string): boolean => {
+  return value === "input";
+};
+
+const evaluateInputKeyword = (context: { input: string }): string => {
+  return context.input;
 };
